86e81a1828ba8598cb1300e2aefafb563e2e4c7c
RachmanForniandi <rachmanforniandi@gmail.com>
2019-09-22 16:48:31 +0700

    change overall into list product look like

--- app/src/main/java/id/com/rxapp/MainActivity.java
+++ app/src/main/java/id/com/rxapp/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.Observable;
@@ -34,17 +37,22 @@ public class MainActivity extends AppCompatActivity {
 
         listData.setLayoutManager(layoutManager);
         listData.setAdapter(rxAdapter);
-        Observable.just("What's up dude?").subscribe(new Consumer<String>() {
+        Observable.just("List products & Prices").subscribe(new Consumer<String>() {
             @Override
             public void accept(String s) throws Exception {
                 tV1.setText(s);
             }
         });
 
-        Observable.just("Mordecai","Rigby","Skips","Muscle Man","Benson","Hi 5 Ghost","Thomas").subscribe(new Consumer<String>() {
+        Entry entry1 = new Entry("Playstation 4", BigDecimal.valueOf(1500),new Date());
+        Entry entry2 = new Entry("Xbox One", BigDecimal.valueOf(2000),new Date());
+        Entry entry3 = new Entry("Xbox One S", BigDecimal.valueOf(2500),new Date());
+        Entry entry4 = new Entry("Xbox One X", BigDecimal.valueOf(3500),new Date());
+        Entry entry5 = new Entry("Sony Xperia XZ", BigDecimal.valueOf(2500),new Date());
+        Observable.just(entry1,entry2,entry3,entry4,entry5).subscribe(new Consumer<Entry>() {
             @Override
-            public void accept(String s) throws Exception {
-                rxAdapter.addStringToList(s);
+            public void accept(Entry entry) throws Exception {
+                rxAdapter.addStringToList(entry);
             }
         });
 
--- app/src/main/java/id/com/rxapp/RxAdapter.java
+++ app/src/main/java/id/com/rxapp/RxAdapter.java
@@ -1,5 +1,6 @@
 package id.com.rxapp;
 
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,7 +9,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import butterknife.BindView;
@@ -17,7 +22,7 @@ import butterknife.ButterKnife;
 public class RxAdapter extends RecyclerView.Adapter<RxAdapter.RxHolder> {
 
 
-    private final List<String> stringValues=new ArrayList<>();
+    private final List<Entry> entries =new ArrayList<>();
 
 
     @NonNull
@@ -29,28 +34,61 @@ public class RxAdapter extends RecyclerView.Adapter<RxAdapter.RxHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull RxAdapter.RxHolder holder, int position) {
-        holder.txtName.setText(stringValues.get(position));
+        //holder.txtName.setText(entries.get(position));
+        Entry entry = entries.get(position);
+        holder.setTxtName(entry.getEntryName());
+        holder.setTxtPrice(entry.getEntryPrice());
+        holder.setTxtDate(entry.getEntryDate());
+
     }
 
     @Override
     public int getItemCount() {
-        return stringValues.size();
+        return entries.size();
     }
 
-    public void addStringToList(String value){
-        stringValues.add(value);
-        notifyItemInserted(stringValues.size()-1);
+    public void addStringToList(Entry entry){
+        entries.add(entry);
+        notifyItemInserted(entries.size()-1);
     }
 
 
     public class RxHolder extends RecyclerView.ViewHolder {
 
+
         @BindView(R.id.txt_name)
         TextView txtName;
+
+        @BindView(R.id.txt_price)
+        TextView txtPrice;
+
+        @BindView(R.id.txt_date)
+        TextView txtDate;
+
+        private final NumberFormat FORMAT_PRICE = new DecimalFormat("$#0.00");
 
         public RxHolder(@NonNull View itemView) {
             super(itemView);
             ButterKnife.bind(this,itemView);
         }
+
+        public TextView getTxtName() {
+            return txtName;
+        }
+
+        public void setTxtName(String en) {
+            this.txtName.setText(en);
+        }
+
+
+        public void setTxtPrice(BigDecimal ep) {
+            this.txtPrice.setText(FORMAT_PRICE.format(ep));
+        }
+
+
+        public void setTxtDate(Date ed) {
+            this.txtDate.setText(android.text.format.DateFormat.format("yyyy-MM-dd hh:mm",ed));
+        }
+
     }
 }
