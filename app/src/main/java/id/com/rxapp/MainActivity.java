dfdb70cf8b4c22fbe52a50752806cbef3eaccb49
RachmanForniandi <rachmanforniandi@gmail.com>
2019-09-22 17:13:57 +0700

    changes some function using lambda expression

--- app/src/main/java/id/com/rxapp/MainActivity.java
+++ app/src/main/java/id/com/rxapp/MainActivity.java
@@ -37,24 +37,26 @@ public class MainActivity extends AppCompatActivity {
 
         listData.setLayoutManager(layoutManager);
         listData.setAdapter(rxAdapter);
-        Observable.just("List products & Prices").subscribe(new Consumer<String>() {
+        /*Observable.just("List products & Prices").subscribe(new Consumer<String>() {
             @Override
             public void accept(String s) throws Exception {
                 tV1.setText(s);
             }
-        });
+        });*/
+        Observable.just("List products & Prices").subscribe(tV1::setText);
 
         Entry entry1 = new Entry("Playstation 4", BigDecimal.valueOf(1500),new Date());
         Entry entry2 = new Entry("Xbox One", BigDecimal.valueOf(2000),new Date());
         Entry entry3 = new Entry("Xbox One S", BigDecimal.valueOf(2500),new Date());
         Entry entry4 = new Entry("Xbox One X", BigDecimal.valueOf(3500),new Date());
         Entry entry5 = new Entry("Sony Xperia XZ", BigDecimal.valueOf(2500),new Date());
-        Observable.just(entry1,entry2,entry3,entry4,entry5).subscribe(new Consumer<Entry>() {
+        /*Observable.just(entry1,entry2,entry3,entry4,entry5).subscribe(new Consumer<Entry>() {
             @Override
             public void accept(Entry entry) throws Exception {
                 rxAdapter.addStringToList(entry);
             }
-        });
+        });*/
+        Observable.just(entry1,entry2,entry3,entry4,entry5).subscribe(entry -> rxAdapter.addStringToList(entry));
 
     }
 }
